191af3ec48c519fb393da18c59a2919b6d161c96
Joao Antonio Chagas Nunes <joaoantoniocn@gmail.com>
2018-12-08 22:58:45 -0300

    added epsilon on formulas

--- speaker_id.py
+++ speaker_id.py
@@ -64,11 +64,12 @@ class AdditiveMarginSoftmax(nn.Module):
 
         self.m = margin #
         self.s = s
+        self.epsilon = 0.000000000001
 
     def forward(self, predicted, target):
 
         # ------------ AM Softmax ------------ #
-        predicted = predicted / predicted.norm(p=2, dim=0)
+        predicted = predicted / (predicted.norm(p=2, dim=0) + self.epsilon)
         indexes = range(predicted.size(0))
         cos_theta_y = predicted[indexes, target]
         cos_theta_y_m = cos_theta_y - self.m
@@ -76,7 +77,7 @@ class AdditiveMarginSoftmax(nn.Module):
 
         sum_cos_theta_j = (np.e ** (predicted * self.s)).sum(dim=1) - (np.e ** (predicted[indexes, target] * self.s))
 
-        log = -torch.log(exp_s/(exp_s+sum_cos_theta_j)).mean()
+        log = -torch.log(exp_s/(exp_s+sum_cos_theta_j+self.epsilon)).mean()
 
         return log
 
